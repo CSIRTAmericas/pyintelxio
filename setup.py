fa6c4b691fd2231d690de38b54a1079aa977e4a2
Einar Lanfranco <einar.lanfranco@gmail.com>
2023-06-26 16:26:29 -0300

    version mistamtch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyintelx',
-    version='0.0.5',
+    version='0.0.6',
     description='This lib add support to use the Identity API from Intelx.io',
     install_requires=['requests'],
     license='MIT',
