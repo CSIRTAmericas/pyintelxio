d1d7b87a07e875c8012cb0c82c4b517435dbcdba
Einar Lanfranco <einar.lanfranco@gmail.com>
2023-06-28 12:46:56 -0300

    litte fix to prevent Errors

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyintelx',
-    version='0.1.12',
+    version='0.2.0',
     description='This lib add support to use the Identity API from Intelx.io',
     license='MIT',
     keywords=['python, package, distribution'],
