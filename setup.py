e7030df9f83e52fe0b92ee0a3a1afa79d1ff0869
gcarrilao <gcarrilao@linti.unlp.edu.ar>
2023-06-28 15:50:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyintelx',
-    version='0.1.4',
+    version='0.1.6',
     description='This lib add support to use the Identity API from Intelx.io',
     license='MIT',
     keywords=['python, package, distribution'],
