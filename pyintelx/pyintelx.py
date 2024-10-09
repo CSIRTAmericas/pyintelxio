f21a3a0836201899b7923f9393677a5dfd3207cc
Einar Lanfranco <einar.lanfranco@gmail.com>
2024-10-09 11:21:01 -0300

    agregando excepciones y timeout a las conexiones, por defecto sera 10 segundos ahora

--- pyintelx/pyintelx.py
+++ pyintelx/pyintelx.py
@@ -5,20 +5,31 @@ import requests
 
 class IdentityService(intelx):
 
-    def __init__(self, api_key, user_agent='IX-Python/0.5'):
+    def __init__(self, api_key, user_agent='IX-Python/0.5', timeout=10, pause_between_requests=1):
         super().__init__(api_key, user_agent)
         self.API_ROOT = 'https://3.intelx.io'
         self.HEADERS = {'X-Key': self.API_KEY, 'User-Agent': self.USER_AGENT}
-        self.PAUSE_BETWEEN_REQUESTS = 1
+        self.TIMEOUT = timeout
+        self.PAUSE_BETWEEN_REQUESTS = pause_between_requests
 
     def get_search_results(self, id, format=1, maxresults=100):
         params = {'id': id, 'format': format, 'limit': maxresults}
-        r = requests.get(self.API_ROOT + '/live/search/result',
-                         params, headers=self.HEADERS)
-        if r.status_code == 200:
-            return r.json()
-        else:
-            return r.status_code
+        try:
+            # Solicitud HTTP con timeout
+            r = requests.get(self.API_ROOT + '/live/search/result',
+                             params=params, headers=self.HEADERS, timeout=self.TIMEOUT)
+            r.raise_for_status()  # Lanzará una excepción si el código no es 200
+
+            return r.json()  # Devolver JSON si la solicitud es exitosa
+        except requests.exceptions.HTTPError as errh:
+            print("Error HTTP:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error de conexión:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Tiempo de espera agotado:", errt)
+        except requests.exceptions.RequestException as err:
+            print("Error en la solicitud:", err)
+        return None  # Retorna None si ocurre un error
 
     def search(self, term, maxresults=100, buckets=[], timeout=5, datefrom="", dateto="",
                terminate=[], analyze=False, skip_invalid=False):
@@ -34,24 +45,37 @@ class IdentityService(intelx):
         }
         done = False
         results = []
-        r = requests.get(self.API_ROOT + '/live/search/internal',
-                         headers=self.HEADERS, params=p)
-        if r.status_code == 200:
-            search_id = r.json()['id']
-        else:
+
+        try:
+            r = requests.get(self.API_ROOT + '/live/search/internal',
+                             headers=self.HEADERS, params=p, timeout=self.TIMEOUT)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError as errh:
+            print("Error HTTP:", errh)
             return (r.status_code, r.text)
-        if (len(str(search_id)) <= 3):
-            print(
-                f"[!] intelx.IDENTITY_SEARCH() Received {self.get_error(search_id)}")
+        except requests.exceptions.ConnectionError as errc:
+            print("Error de conexión:", errc)
+            return "Error de conexión"
+        except requests.exceptions.Timeout as errt:
+            print("Tiempo de espera agotado:", errt)
+            return "Tiempo de espera agotado"
+        except requests.exceptions.RequestException as err:
+            print("Error en la solicitud:", err)
+            return "Error en la solicitud"
+
+        search_id = r.json().get('id')
+        if len(str(search_id)) <= 3:
+            print(f"[!] intelx.IDENTITY_SEARCH() Received {self.get_error(search_id)}")
+
         while not done:
             time.sleep(self.PAUSE_BETWEEN_REQUESTS)
             r = self.get_search_results(search_id, maxresults=maxresults)
-            if (r["status"] == 0 and r["records"]):
-                for a in r['records']:
-                    results.append(a)
+            if r and r.get("status") == 0 and r.get("records"):
+                results.extend(r['records'])
                 maxresults -= len(r['records'])
-            if (r['status'] == 2 or maxresults <= 0):
-                if (maxresults <= 0):
+
+            if r and (r.get("status") == 2 or maxresults <= 0):
+                if maxresults <= 0:
                     self.terminate_search(search_id)
                 done = True
         return {'records': results}
@@ -60,57 +84,98 @@ class IdentityService(intelx):
         p = {
             "id": id,
         }
-        r = requests.get(self.API_ROOT + '/live/search/terminate',
-                         headers=self.HEADERS, params=p)
-        if r.status_code == 204:
+        try:
+            r = requests.get(self.API_ROOT + '/live/search/terminate',
+                             headers=self.HEADERS, params=p, timeout=self.TIMEOUT)
+            r.raise_for_status()
             return (r.status_code, r.text)
-        else:
-            return (r.status_code, r.text)
+        except requests.exceptions.HTTPError as errh:
+            print("Error HTTP:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error de conexión:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Tiempo de espera agotado:", errt)
+        except requests.exceptions.RequestException as err:
+            print("Error en la solicitud:", err)
+        return None  # Retorna None si ocurre un error
 
     def export_accounts(self, term, datefrom=None, dateto=None, maxresults=10, buckets=[], terminate=None):
         p = {
             "selector": term,
             "bucket": buckets,
             "limit": maxresults,
-            "datefrom": datefrom,  # "YYYY-MM-DD HH:MM:SS",
-            "dateto": dateto,  # "YYYY-MM-DD HH:MM:SS"
+            "datefrom": datefrom,  # "YYYY-MM-DD HH:MM:SS"
+            "dateto": dateto,      # "YYYY-MM-DD HH:MM:SS"
             "terminate": terminate,
         }
+        
+        # Limpiar parámetros opcionales que sean None
+        p = {k: v for k, v in p.items() if v is not None}
+
         done = False
         results = []
-        r = requests.get(self.API_ROOT + '/accounts/csv',
-                         headers=self.HEADERS, params=p)
-        if r.status_code == 200:
-            search_id = r.json()['id']
-            if (len(str(search_id)) <= 3):
-                print(
-                    f"[!] intelx.IDENTITY_EXPORT() Received {self.get_error(search_id)}")
+   
+        try:
+            # Primera solicitud a /accounts/csv con timeout
+            r = requests.get(self.API_ROOT + '/accounts/csv',
+                             headers=self.HEADERS, params=p, timeout=self.TIMEOUT)
+            r.raise_for_status()  # Verifica si la solicitud fue exitosa (código 200)
+
+            search_id = r.json().get('id')  # Obtiene el ID de la búsqueda
+            if not search_id or len(str(search_id)) <= 3:
+                print(f"[!] intelx.IDENTITY_EXPORT() Received {self.get_error(search_id)}")
+                return
+
+            # Loop para obtener resultados de búsqueda
             while not done:
                 time.sleep(self.PAUSE_BETWEEN_REQUESTS)
                 r = self.get_search_results(search_id, maxresults=maxresults)
-                if (r["status"] == 0 and r["records"]):
-                    for a in r['records']:
-                        results.append(a)
+
+                if r and r.get("status") == 0 and r.get("records"):
+                    results.extend(r['records'])
                     maxresults -= len(r['records'])
-                if (r['status'] == 2 or maxresults <= 0):
-                    if (maxresults <= 0):
+
+                # Si el estado es 2 o se alcanzan los resultados deseados, termina la búsqueda
+                if r and (r.get("status") == 2 or maxresults <= 0):
+                    if maxresults <= 0:
                         self.terminate_search(search_id)
                     done = True
+
             return {'records': results}
-        else:
-            return (r.status_code, r.text)
+
+        except requests.exceptions.HTTPError as errh:
+            print("Error HTTP:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error de conexión:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Tiempo de espera agotado:", errt)
+        except requests.exceptions.RequestException as err:
+            print("Error en la solicitud:", err)
+
+        return None  # Retorna None si ocurre un error
+   
+   
 
     def export_csv(self, selector, date_from=None, date_to=None, limit=10, bucket_filter=[], terminate=None):
         p = {
             "selector": selector,
             "bucket": bucket_filter,
             "limit": limit,
-            "datefrom": date_from, # "YYYY-MM-DD HH:MM:SS",
-            "dateto": date_to, # "YYYY-MM-DD HH:MM:SS"
+            "datefrom": date_from,  # "YYYY-MM-DD HH:MM:SS",
+            "dateto": date_to,  # "YYYY-MM-DD HH:MM:SS"
             "terminate": terminate,
         }
-        r = requests.get(self.API_ROOT + '/accounts/csv', headers=self.HEADERS, params=p)
-        if r.status_code == 200:
-            return r.json()['id']
-        else:
-            return (r.status_code, r.text)
+       try:
+            r = requests.get(self.API_ROOT + '/accounts/csv',
+                             headers=self.HEADERS, params=p, timeout=self.TIMEOUT)
+            r.raise_for_status()  # Verifica si la solicitud fue exitosa (código 200)
+            return r.json().get('id')  # Devuelve el 'id' si la solicitud es exitosa
+        except requests.exceptions.HTTPError as errh:
+            print("Error HTTP:", errh)
+        except requests.exceptions.ConnectionError as errc:
+            print("Error de conexión:", errc)
+        except requests.exceptions.Timeout as errt:
+            print("Tiempo de espera agotado:", errt)
+        except requests.exceptions.RequestException as err:
+            print("Error en la solicitud:", err)
+        return None  # Retorna None si ocurre un error
